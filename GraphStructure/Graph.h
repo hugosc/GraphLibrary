440d6bfdaa704b87964c552171a90371fb71cfb8
hugosc <hugosalescorrea@gmail.com>
2014-10-23 02:55:46 -0300

    Just because the message can't be empty. Howdy!

--- GraphStructure/Graph.h
+++ GraphStructure/Graph.h
@@ -1,4 +1,13 @@
 #include <vector>
+#include <stdlib.h>
+
+#ifndef Graph_H_
+#define Graph_H_
+
+#define WHITE 0
+#define GREY 1
+#define BLACK 2
+
 
 struct edge {
     int key;
@@ -10,20 +19,48 @@ struct edge {
 
 template <class T> class node {
     public :
-         node (T*, node*, int);
-         node (T*, node*);
+         node (T, node*, int);
          node~ (void);
          T getInfo (void);
-         int getColor (void);
-         node* getParent (void);
          int getDegree (void);
-         void addEdge (int);
+         void addEdge (int, int);
          void deleteEdge (int);
 
+         int color;
+         node<T>* parent;
+
     private :
-        int color, degree;
-        node* parent;
+        int degree;
+        T info;
         edge* adj_list;
+}
+
+template <class T> node<T>::node<T> (T data, node* prt, int clr) {
+    info = data;
+    parent = prt;
+    color = clr;
+    degree = 0;
+}
+
+template <class T> T node<T>::getInfo () {
+    return info;
+}
+
+template <class T> int node<T>::getDegree () {
+    return degree;
+}
+
+template <class T> void addEdge (int s, int weight) {
+    edge* new_edge = new edge;
+    new_edge->key = s;
+    new_edge->weight = weight;
+
+    new_edge->next = adj_list;
+    adj_list = new_edge;
+}
+
+template <class T> void deleteEdge (int s) {
+    
 }
 
 template <class T> class Graph {
@@ -41,3 +78,5 @@ template <class T> class Graph {
         int ne;
         std::vector<node<T>> v_list;
 }
+
+#endif
